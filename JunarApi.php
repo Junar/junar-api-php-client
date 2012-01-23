763ceae3a10ab843e38837a0263f62feb4101427
Joaquín Núñez <contacto@joaquinnunez.cl>
2012-01-23 18:32:02 -0300

    [Format] xml, we will return an xml raw string, use your favourite library to handle the xml

--- JunarApi.php
+++ JunarApi.php
@@ -65,6 +65,7 @@ class DataStream {
          *         - csv
          *         - tsv
          *         - excel
+         *         - xml, prettyjson sister, configuration is not mandatory
          */
 
         if ($this->authkey == '')
